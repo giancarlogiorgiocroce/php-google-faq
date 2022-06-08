ebc7f5c7caea6abadc3f7d3226c855e8f0521d8b
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-08 16:15:30 +0200

    Aggiunto Footer

--- index.php
+++ index.php
@@ -29,16 +29,59 @@ $db = [
         ",
     ],
     [
-        'question' => "Ciccio",
-        'answer' => "Pasticcio",
+        'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        'answer' => "
+            <p>
+                Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+            </p>
+            <p>
+                Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
+            </p>
+            <p> 
+                Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href=''>Centro Google per la sicurezza online</a>.
+                
+                <a href=''>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.
+            </p>
+        ",
     ],
     [
-        'question' => "Ciccio",
-        'answer' => "Pasticcio",
-    ],
-    [
-        'question' => "Ciccio",
-        'answer' => "Pasticcio",
+        'question' => "Perché il mio account è associato a un paese?",
+        'answer' => "
+            <p>
+                Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+            </p>
+            <ul>
+                <li>
+                    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                    <ul>
+                        <li>
+                            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+                        </li>
+                        <li>
+                            Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+                        </li>
+                    </ul>      
+                </li>
+                <li>
+                    La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+                </li>
+            </ul>
+            <p>
+                Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
+            </p>
+            <h5>
+                Stabilire il paese associato al tuo account
+            </h5>
+            <p>
+                Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
+            </p>
+            <p>
+                I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+            </p>
+            <p>
+                Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href=''>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.
+            </p>
+        ",
     ],
 ]
 
@@ -62,6 +105,8 @@ $db = [
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="general.css">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -73,12 +118,38 @@ $db = [
         <div class="wrapper">
             <?php foreach($db as $section => $QnA): ?>
                 <section class="container-fluid">
-                    <h2><?php echo $QnA['question'] ?></h2>
+                    <h3><?php echo $QnA['question'] ?></h3>
                     <div><?php echo $QnA['answer'] ?></div>
                 </section>
             <?php endforeach; ?>
         </div>
-    </main> 
+    </main>
+
+    <footer>
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-3">
+                        <ul class="d-flex justify-content-evenly">
+                            <li><a href="#">Google</a></li>
+                            <li><a href="#">Tutto su Google</a></li>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Termini</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3 d-flex justify-content-end aling-items-center">
+                        <i class="fa-solid fa-globe"></i>
+                        <select name="" id="">
+                            <option value="">Italiano</option>
+                            <option value="">Tedesco</option>
+                            <option value="">Inglese</option>
+                            <option value="">Klingon</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </body>
 </html>
